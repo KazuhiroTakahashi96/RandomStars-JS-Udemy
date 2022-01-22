3adc6ee4190366f6526bae17a9619039fe53ab78
Kazu Takahashi <97425700+KazuhiroTakahashi96@users.noreply.github.com>
2022-01-22 17:17:36 +0900

    Add files via upload

---
+++ app.js
@@ -0,0 +1,96 @@
+// https://www.mathsisfun.com/
+
+const canvasEl = document.querySelector("canvas");
+const canvasContext = canvasEl.getContext("2d");
+
+canvasEl.width = window.innerWidth;
+canvasEl.height = window.innerHeight;
+
+const starsColor = "white";
+const starsNumber = 1000;
+const size = 0.005; // maximum star size as a fraction of the screen width
+const speed = 0.05; // fraction of screen width per second
+
+// Setting up the stars
+let stars = [];
+let starsSpeed = speed * canvasEl.width;
+let horizontalVelocity = starsSpeed * randomSign() * Math.random();
+let verticalVelocity =
+  Math.sqrt(Math.pow(starsSpeed, 2) - Math.pow(horizontalVelocity, 2)) *
+  randomSign();
+// a = sqrt(c^2 - b^2)
+
+// randomizing the stars speed, size & location
+for (let i = 0; i < starsNumber; i++) {
+  let speedBoost = Math.random() * 2.5 + 0.5;
+  stars[i] = {
+    starsRadius: (Math.random() * size * canvasEl.width) / 2,
+    horizontalPosition: Math.floor(Math.random() * canvasEl.width),
+    verticalPosition: Math.floor(Math.random() * canvasEl.height),
+    horizontalVelocity: horizontalVelocity * speedBoost,
+    verticalVelocity: verticalVelocity * speedBoost,
+  };
+}
+
+//--------------------- The Game Loop
+
+// Animation Loop
+let timeDiff,
+  timeLast = 0;
+requestAnimationFrame(runStars);
+
+function runStars(timeNow) {
+  canvasContext.clearRect(0, 0, canvasEl.width, canvasEl.height);
+  // calculate the frame difference
+  timeDiff = timeNow - timeLast;
+  timeLast = timeNow;
+
+  // Drawing stars
+  canvasContext.fillStyle = starsColor;
+  for (let i = 0; i < starsNumber; i++) {
+    canvasContext.beginPath();
+    canvasContext.arc(
+      stars[i].horizontalPosition,
+      stars[i].verticalPosition,
+      stars[i].starsRadius,
+      0,
+      Math.PI * 2
+    );
+    canvasContext.fill();
+
+    // update stars horizontal position
+    stars[i].horizontalPosition +=
+      stars[i].horizontalVelocity * timeDiff * 0.001;
+
+    // reposition stars to the other side of screen when they move off either side of the screen
+    if (stars[i].horizontalPosition < 0 - stars[i].starsRadius) {
+      stars[i].horizontalPosition = canvasEl.width + stars[i].starsRadius;
+    } else if (
+      stars[i].horizontalPosition >
+      canvasEl.width + stars[i].starsRadius
+    ) {
+      stars[i].horizontalPosition = 0 - stars[i].starsRadius;
+    }
+
+    //
+    // update stars vertical position
+    stars[i].verticalPosition += stars[i].verticalVelocity * timeDiff * 0.001;
+
+    // reposition stars to top/bottom side of screen when they move off either from the top or bottom
+    if (stars[i].verticalPosition < 0 - stars[i].starsRadius) {
+      stars[i].verticalPosition = canvasEl.height + stars[i].starsRadius;
+    } else if (
+      stars[i].verticalPosition >
+      canvasEl.height + stars[i].starsRadius
+    ) {
+      stars[i].verticalPosition = 0 - stars[i].starsRadius;
+    }
+  }
+
+  requestAnimationFrame(runStars);
+}
+
+// randomSign Function
+function randomSign() {
+  return Math.random() >= 0.5 ? 1 : -1;
+}
